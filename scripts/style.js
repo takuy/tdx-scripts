58ae1baaaacaf27a3e488254580b4974111528ce
Sam <1631095+takuy@users.noreply.github.com>
2025-05-23 22:34:22 -0400

    Update style.js

--- scripts/style.js
+++ scripts/style.js
@@ -1,4 +1,4 @@
-function doTabulatorThings() {
+function tabulatorSoftwareTable() {
     window.$table = new Tabulator('table[name="software_list"]', {
         layout:"fitColumns",
         columns: [
@@ -26,16 +26,26 @@ function doTabulatorThings() {
 jQuery(document).ready(function() {
     $('head').append('<link rel="stylesheet" type="text/css" href="https://unpkg.com/tabulator-tables@6.3.1/dist/css/tabulator_bootstrap3.min.css">');
     $('head').append('<link rel="stylesheet" type="text/css" href="https://takuy.github.io/tdx-scripts/style/style.css">');
-    
-    doTabulatorThings();
 
-    jQuery(document, 'div[name="role-filter"] input:checked').on('change', function() {
-    let roles = jQuery('div[name="role-filter"] input:checked').map(function() { return $(this).val();}).get();
-    $('span[name="software-filter-list"]').text(roles.length > 0 ? roles.join(", ") : $('span[name="software-filter-list"]').data("default") );
-        $table.setFilter(function(data, params) {
-            let match = data["audience"].split(", ").some(r=> params.roles.includes(r));
-            return params.roles.length > 0 ? match : 1;
-        }, {roles: roles});
-    });
-  
+    if(jQuery('table[name="software_list"]').length) {
+        tabulatorSoftwareTable();
+    
+        jQuery(document, 'div[name="role-filter"] input:checked').on('change', function() {
+        let roles = jQuery('div[name="role-filter"] input:checked').map(function() { return $(this).val();}).get();
+        $('span[name="software-filter-list"]').text(roles.length > 0 ? roles.join(", ") : $('span[name="software-filter-list"]').data("default") );
+            $table.setFilter(function(data, params) {
+                let match = data["audience"].split(", ").some(r=> params.roles.includes(r));
+                return params.roles.length > 0 ? match : 1;
+            }, {roles: roles});
+        });
+    }
+
+    if(jQuery('table.simple-tabulator').length) {
+        var toTabulatorify = jQuery('table.simple-tabulator');
+        toTabulatorify.forEach(function(tabl) {
+            new Tabulator(tabl, {
+                layout:"fitColumns"
+          });
+        }
+    }
 });
