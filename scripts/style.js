230758b7efea13eb5910a75f97e38c7eee17d961
Sam <1631095+takuy@users.noreply.github.com>
2025-05-29 23:56:44 -0400

    Update style.js

--- scripts/style.js
+++ scripts/style.js
@@ -98,7 +98,7 @@ jQuery(document).ready(function() {
         
       $('select[name="role-filter"], select[name="platform-filter"], select[name="category-filter"]').select2({
         placeholder: 'Click to select', 
-        allowClear: true, multiple: true, width: "style"
+        allowClear: true, width: "style"
       });
       
       if(jQuery('select[name="role-filter"]').length) {
