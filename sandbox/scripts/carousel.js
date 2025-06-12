034b074f90ace9fb5619db46e7113e16b1fe0460
Sam <1631095+takuy@users.noreply.github.com>
2025-06-12 15:21:39 -0400

    adding sandbox folder

---
+++ sandbox/scripts/carousel.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function() {
+  jQuery('.carousel').on('slide.bs.carousel', function (event) {
+      var current_slide = event.relatedTarget;
+      var slide_id = jQuery(event.relatedTarget).data("slide-id");
+      var parent_carousel = jQuery(event.target).attr("class").replace("carousel ", "")
+      jQuery(".slide-pages .btn").removeClass("active");
+      jQuery(".slide-pages .btn[data-slide-to='" + slide_id + "'][data-target='." + parent_carousel + "']").addClass("active");
+      console.log(slide_id, parent_carousel);
+  });
+});
---
+++ sandbox/scripts/style.js
@@ -0,0 +1,238 @@
+function tabulatorSoftwareTable() {
+    // make the table page full width
+    $('#divMainContent').addClass('col-12').removeClass("col-md-8");
+    //$('#divMainContent + div.col-md-4').hide();
+    
+    window.$table = new Tabulator('table[name="software_list"]', {
+        layout:"fitDataStretch",
+        pagination:true,
+        paginationSize:10,
+        paginationSizeSelector:[5, 10, 25, 50, 100, true],
+        paginationCounter: "rows",
+        columns: [
+            {
+                title: "Cost",
+                formatter: function(cell) {
+                    return cell.getValue() == "$" ? "<span role='img' title='There is a cost associated with this software.'>💲</span>" : "";
+                }, hozAlign: "center", width: 40, resizable: false,
+                titleFormatter: function() {
+                    return "&nbsp;";
+                }
+            }, {
+                title: "SoftwareLink", resizable: false,
+                formatter: "html", visible: false
+            }, {
+                field: "softwaretitle", title: "Software", resizable: false, formatter: "link", formatterParams: {
+                  target: "_blank",
+                  url: function(cell) {
+                    return cell.getRow().getData()["servicelink"];
+                  },
+                  label: function(cell) { 
+                    return cell.getRow().getData()["softwaretitle"];
+                  }
+                }
+            }, {
+                title: "ServiceLink", resizable: false, visible: false
+            }, {
+                title: "SoftwareTitle", resizable: false, visible: false, formatter: "html"
+            }, {
+                title: "Category", resizable: false
+            }, {
+                title: "Audience", resizable: false
+            }, {
+                title: "Platform", resizable: false
+            }, {
+                title: "Description", resizable: false, visible: false
+            }
+        ]
+    });   
+
+    let filterableItems = [{ 
+            "column": "audience",
+            "filter": "role",
+        }, { 
+            "column": "platform",  
+            "filter": "platform", 
+        }, { 
+            "column": "category",
+            "filter": "category" 
+        }
+    ];
+
+    let allFilterableSelectors = [];
+    let allFilterableSelectorsText = "";
+    
+    filterableItems.forEach(function(t) {
+        let filterSelector = `select[name="${t.filter}-filter"]`;
+        let filterDescriber = `span[name="filtered-${t.filter}"]`;
+        t.selector = filterSelector;
+        t.describer = filterDescriber;
+
+        allFilterableSelectors.push(filterSelector);
+        
+        $(filterSelector).each(function() {
+            $(this).attr('id', $(this).attr('name'));
+            $(this).select2({
+                placeholder: 'Click to select', 
+                allowClear: true, width: "200px"
+            });
+        });
+        
+    });
+
+    allFilterableSelectorsText = allFilterableSelectors.join(", ");
+
+    $('button[name="filter-reset"]').click(function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        $(allFilterableSelectorsText).val(null).trigger('change');
+        $('input[name="software-search"]').val(null);
+        updateSoftwareFilter(filterableItems);
+    });
+
+    $(document, allFilterableSelectorsText).on('change.select2 select2:select select2:clear', function() {
+        updateSoftwareFilter(filterableItems);
+    });
+
+    document.querySelector('input[name="software-search"]').addEventListener("keyup", function() { 
+        updateSoftwareFilter(filterableItems) 
+    });
+    $('input[name="software-search"]').attr('id', $('input[name="software-search"]').attr('name'));
+}
+
+var tdx_utils = {
+    setFilterDescription: function(array, selector) {
+        $(selector).text(array.length > 0 ? array.join(", ") : $(selector).data("default") );
+    },
+    getSelectedItems: function(selector) {
+        return $(selector).select2('data').map(function(t) { return t.id });
+    },
+    hasFilterMatch: function(selected, data) {
+        return (selected.length) > 0 ? data.split(", ").some(r=> selected.includes(r)) : 1;
+    },
+    getAllSelectors: function(filterableItems) {
+        return filterableItems.map((t) => t.selector);
+    }
+};
+
+function updateSoftwareFilter(filterableItems) {
+    filterableItems.forEach(function(t) {
+        // array of selected items
+        let selected = tdx_utils.getSelectedItems(t.selector);
+        tdx_utils.setFilterDescription(selected, t.describer);
+        t.selected = selected;
+    });
+
+    let filterText = jQuery('input[name="software-search"]').val();
+
+    $table.setFilter(function(data, params) {
+        let hasFilterablesMatch = true;
+        params.filterables.forEach(function(t) {
+            hasFilterablesMatch &= tdx_utils.hasFilterMatch(t.selected, data[t.column]);
+        });
+      
+        let hasTextMatch = false;
+        if (params.filterText) {
+            for (prop in data) {
+                if(prop == "id") { continue; }
+                if (data[prop] && data[prop].toLowerCase().indexOf(params.filterText.toLowerCase()) > -1) {
+                    hasTextMatch |= true;
+                }
+            }
+        } else {
+            hasTextMatch = true;
+        }
+      
+        return hasFilterablesMatch && hasTextMatch;
+    }, { filterables: filterableItems , filterText: filterText});
+}
+
+function tabulatorGroupTable() {
+    window.$table = new Tabulator('table[name="group-table"]', {
+        layout:"fitDataStretch",
+        columns: [
+            {
+                title: "Helix Support Group", resizable: false, formatter: "html",
+                headerFilter: "input", headerFilterPlaceholder: "Type to filter..."
+            }, {
+                title: "TDX Group Name", resizable: false, formatter: "html",
+                headerFilter: "input", headerFilterPlaceholder: "Type to filter..."
+            }
+        ]
+    });   
+
+    $('#divMainContent').addClass('col-12').removeClass("col-md-8");
+
+    document.querySelector('input[name="software-search"]').addEventListener("keyup", updateSoftwareFilter);
+}
+
+function updateGroupTableFilter() {
+    let filterText = jQuery('input[name="group-search"]').val();  
+    $table.setFilter(function(data, params) {
+        let hasTextMatch = false;
+        if (params.filterText) {
+            for (prop in data) {
+                if(prop == "id") { continue; }
+                if (data[prop] && data[prop].toLowerCase().indexOf(params.filterText.toLowerCase()) > -1) {
+                    hasTextMatch |= true;
+                }
+            }
+        } else {
+            hasTextMatch = true;
+        }
+      
+        return  hasTextMatch;
+    }, {filterText: filterText});
+}
+
+jQuery(document).ready(function() {
+    $('head').append('<link rel="stylesheet" type="text/css" href="https://takuy.github.io/tdx-scripts/sandbox/style/style.css">');
+    $('head').append('<link rel="stylesheet" type="text/css" href="https://unpkg.com/tabulator-tables@6.3.1/dist/css/tabulator_bootstrap3.min.css">');
+    $('head').append('<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/tocbot/4.34.0/tocbot.css">');
+   // $('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css">');
+    if(jQuery('#divAttachments .js-attachments-body').data('attachmentsCount') == 0) {
+        jQuery('#divAttachments').hide();
+    }
+    
+    if(jQuery('table[name="software_list"]').length) {
+        tabulatorSoftwareTable();
+    }
+
+    if(jQuery('table[name="group-table"]').length) {
+        tabulatorGroupTable();
+    }
+    
+    if(jQuery('table.simple-tabulator').length) {
+        var toTabulatorify = jQuery('table.simple-tabulator');
+        toTabulatorify.each(function() {
+            if ($(toTabulatorify).hasClass("tbl-full-width")) {
+                $('#divMainContent').addClass('col-12').removeClass("col-md-8");
+            } 
+            if ($(toTabulatorify).hasClass("hide-sidebar")) {
+                $('#divMainContent + div.col-md-4').hide();
+            }
+            new Tabulator(this, {
+                layout:"fitData"
+            });
+        });
+    }
+    
+
+    $('h1,h2,h3,h4,h5').each(function() {
+        if(!$(this).attr('id')) {
+            let new_id_source = $(this).attr('name') || $(this).text();
+            let new_id = new_id_source.replaceAll(/[^A-Za-z0-9_-\w]/gm,'_').toLowerCase();
+            $(this).attr('id', new_id)
+        }
+    });
+    tocbot.init({
+        tocSelector: "div[name='toc']",
+        contentSelector: 'div.page',
+        // Which headings to grab inside of the contentSelector element.
+        headingSelector: 'h1, h2, h3',
+        hasInnerContainers: true,
+        headingsOffset: 40,
+        scrollSmoothOffset: -40
+    });
+
+});
