082b8aa9937a91e87d2e799d5cfdaee645ff92a9
Sam <1631095+takuy@users.noreply.github.com>
2025-06-13 13:47:28 -0400

    logging

--- sandbox/scripts/style.js
+++ sandbox/scripts/style.js
@@ -230,6 +230,8 @@ jQuery(document).ready(function() {
 
     $(window).scroll(function(){
         var headerHeight = $('#divMstrHeader').outerHeight(true);
+        console.log($(window).scrollTop());
+        console.log(headerHeight)
         if(($('footer').position()['top'] - $(window).scrollTop() - $('footer').outerHeight()) >  430) {
             $('#tool-sidebar').addClass('end');
             $('#tool-sidebar').removeClass('fixed');
