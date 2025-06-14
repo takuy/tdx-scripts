8861108e2b477b10681f0e11c612c011c3ec34f4
Sam <1631095+takuy@users.noreply.github.com>
2025-06-13 21:45:39 -0400

    maybe?

--- sandbox/scripts/style.js
+++ sandbox/scripts/style.js
@@ -246,7 +246,7 @@ jQuery(document).ready(function() {
     });
 */
     if($("meta[property='og:type']").attr('content') == "article") {
-        $('#ctl00_ctl00_cpContent_cpContent_divTags').append(`
+        $('#divMainContent > h1').append(`
             <div class='temple_toc-parent pull-right' name='toc'>
                 <div class="temple_toc panel panel-default">
                     <div class="panel-heading">Table of Contents</div>
