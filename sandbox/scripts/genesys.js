45ca62cdb0bc924a4af04275ec35a80b831b66b1
Sam <1631095+takuy@users.noreply.github.com>
2025-06-16 14:06:50 -0400

    positioning

--- sandbox/scripts/genesys.js
+++ sandbox/scripts/genesys.js
@@ -8,32 +8,33 @@ jQuery(function() {
         "https://tuhelp.temple.edu/public/chatbottom.js", function() { 
         jQuery.getScript(
             "https://tuhelp.temple.edu/public/webmessaging.js", function() {
-                if(window.Genesys) {
-                    return;
-                }
+                window.initGenesys = function initGenesys() {
+                    if(window.Genesys) {
+                        return;
+                    }
 
-                var id = "e51ca198-18ee-4891-9ae2-29627dd92c16" // dev deployment
-                const currentUrl = window.location.hostname;
+                    var id = "e51ca198-18ee-4891-9ae2-29627dd92c16" // dev deployment
+                    const currentUrl = window.location.hostname;
 
-                if (currentUrl === "help.temple.edu") {
-                    id = "6195ecc5-05bf-4e9b-a977-5bea319e7ee7" // prod deployment
-                }
+                    if (currentUrl === "tuhelp.temple.edu") {
+                        id = "6195ecc5-05bf-4e9b-a977-5bea319e7ee7" // prod deployment
+                    }
 
-                (function (g, e, n, es, ys) {
-                    g['_genesysJs'] = e;
-                    g[e] = g[e] || function () {
-                    (g[e].q = g[e].q || []).push(arguments)
-                    };
-                    g[e].t = 1 * new Date();
-                    g[e].c = es;
-                    ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
-                })(window, 'Genesys', 'https://apps.mypurecloud.com/genesys-bootstrap/genesys.min.js', {
-                    environment: 'use1',
-                    deploymentId: id
-                });
-        })
+                    (function (g, e, n, es, ys) {
+                        g['_genesysJs'] = e;
+                        g[e] = g[e] || function () {
+                            (g[e].q = g[e].q || []).push(arguments)
+                        };
+                        g[e].t = 1 * new Date();
+                        g[e].c = es;
+                        ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+                        })(window, 'Genesys', 'https://apps.mypurecloud.com/genesys-bootstrap/genesys.min.js', {
+                        environment: 'use1',
+                        deploymentId: id
+                    });
+                };
+        });
     });
-
 });
 
 
