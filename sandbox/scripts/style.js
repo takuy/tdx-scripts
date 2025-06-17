fdb03da308d3edb5854418be528a37c05d5d424d
Sam <1631095+takuy@users.noreply.github.com>
2025-06-17 18:29:43 -0400

    another fix?

--- sandbox/scripts/style.js
+++ sandbox/scripts/style.js
@@ -272,7 +272,7 @@ jQuery(document).ready(function() {
         window.addEventListener("scroll", function() {
             var elementTarget = document.querySelector(".topLevelSearch");
             if (window.scrollY > (elementTarget.offsetTop + elementTarget.offsetHeight) && ($(window).width() >= 975) ) {
-                $(elementTarget).append($('#td-navbar-collapse'));
+                $('#td-navbar-collapse').append(elementTarget);
             } else {
                 $('.master-header-right').prepend(elementTarget);
             }
