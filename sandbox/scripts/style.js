3bfd4288b8063a2778e8351c6cce925675b45ef5
Sam <1631095+takuy@users.noreply.github.com>
2025-06-17 23:16:23 -0400

    oops

--- sandbox/scripts/style.js
+++ sandbox/scripts/style.js
@@ -281,7 +281,7 @@ jQuery(document).ready(function() {
             output += `<span class="label label-${type}">${counter[type]}</span>`;
             tooltipOutput += ( tooltipOutput.len > 0 ? ', ' : '' ) + `${type2WordsMap[type]}s: ${counter[type]}`;
         }
-        $("ul li a:contains('System Status')").attr({"data-toggle": "tooltip", "data-placement": "data-bottom", "title": tooltipOutput}).append(output).tooltip()
+        $("ul li a:contains('System Status')").attr({"data-toggle": "tooltip", "data-placement": "bottom", "title": tooltipOutput}).append(output).tooltip()
     });
 
 
